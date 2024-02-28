b44b1283b62cb189c943b346224c4cf2ff1c314b
Fish <115324554+Bwaf@users.noreply.github.com>
2024-02-28 23:21:55 +0000

    fixed major file buggs

---
+++ README.md
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <a href="https://fishuuui.github.io" target="_blank">fishuuui.github.io</a>
+
+    Fishy: I have added the base line of code for the website
+    FishyChris1: Nothing yet
+  </body>
+</html>
---
+++ assets/Encrypter/index.js
@@ -0,0 +1,10 @@
+var password;
+var pass1="fishy";
+password=prompt('Enter Password To View Page','');
+if (password==pass1) {
+    alert('Correct password, Click OK to enter');
+}
+else {
+    alert('Wrong, try again');
+    window.location="https://classroom.google.com"
+};
