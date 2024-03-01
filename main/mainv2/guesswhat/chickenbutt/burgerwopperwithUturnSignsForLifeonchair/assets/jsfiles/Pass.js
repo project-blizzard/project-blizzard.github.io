f99eb2aeb425a3b4709aee2ac97b87532b6b0e39
fishychris1 <161517014+fishychris1@users.noreply.github.com>
2024-02-29 19:48:08 -0800

    change staff link

--- main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Pass.js
+++ main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Pass.js
@@ -1,7 +1,7 @@
 function checkPassword(){
     if(document.getElementById('password').value == 'hello'){ 
        location.href = "http://fishuuui.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
-       alert('correct Passcode!');
+       //alert('correct Passcode!');
       } else {
       alert('Wrong Password!');
        return false;
