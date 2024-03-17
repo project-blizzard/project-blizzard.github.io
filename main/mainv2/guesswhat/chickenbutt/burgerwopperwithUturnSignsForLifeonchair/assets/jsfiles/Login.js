ee30c5f41fb4152c1fe68fd40503e936d089607e
Fishuuui <161544890+Fishuuui@users.noreply.github.com>
2024-03-16 21:49:08 -0700

    adding crit the fur log in info will be changed later

--- main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Login.js
+++ main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Login.js
@@ -5,13 +5,16 @@ function checkUser(){
         location.href = "http://fishuuui.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
         }
       } else {
-      alert('Wrong Password!');
+      alert('Wrong Passcode or Username');
        return false;
      }
 
     if(document.getElementById('username').value == 'crit'){
-        location.href = "http://fishuuui.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
+        if(document.getElementById('passcode').value == 'crit'){
+            location.href = "http://fishuuui.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";  
+        }
     } else {
+        alert('Wrong Passcode or Username');
         return false;
     }
 }
