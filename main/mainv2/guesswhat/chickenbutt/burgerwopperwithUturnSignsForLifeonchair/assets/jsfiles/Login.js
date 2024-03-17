31638aa4a90a065a865f8485545548218a4af802
Fishuuui <161544890+Fishuuui@users.noreply.github.com>
2024-03-16 20:57:44 -0700

    adding another username

--- main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Login.js
+++ main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Login.js
@@ -3,7 +3,12 @@ function checkUser(){
        location.href = "http://fishuuui.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
        //alert('correct Passcode!');
       } else {
-      alert('Wrong Password!');
+      //alert('Wrong Password!');
        return false;
      }
+    if(document.getElementById('username').value == 'crit'){
+        location.href = " https://fishuuui.github.io/main";
+    } else {
+        return false;
+    }
     }
