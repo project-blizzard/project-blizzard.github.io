504e3aebe93414225e0c9af7d2002e020c46d78a
Fishuuui <161544890+Fishuuui@users.noreply.github.com>
2024-03-16 20:50:37 -0700

    linking the login js file to the new sign-in page

---
+++ main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Login.js
@@ -0,0 +1,9 @@
+function checkPassword(){
+    if(document.getElementById('password').value == 'hello'){ 
+       location.href = "http://fishuuui.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
+       //alert('correct Passcode!');
+      } else {
+      alert('Wrong Password!');
+       return false;
+     }
+    }
