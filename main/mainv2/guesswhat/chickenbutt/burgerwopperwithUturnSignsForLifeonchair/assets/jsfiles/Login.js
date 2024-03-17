82640bad5fe44f92332a3a1a09655d3590938f16
Fishuuui <161544890+Fishuuui@users.noreply.github.com>
2024-03-16 21:24:42 -0700

    testing a more simple login system for the signin page

--- main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Login.js
+++ main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Login.js
@@ -1,14 +1,17 @@
 function checkUser(){
     if(document.getElementById('username').value == 'fish'){ 
-       location.href = "http://fishuuui.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
        //alert('correct Passcode!');
+       if(document.getElementById('passcode').value == 'fish'){
+        location.href = "http://fishuuui.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
+        }
       } else {
       //alert('Wrong Password!');
        return false;
      }
+
     if(document.getElementById('username').value == 'crit'){
         location.href = "http://fishuuui.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
     } else {
         return false;
     }
-    }
+}
