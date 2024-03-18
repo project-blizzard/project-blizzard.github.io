4adda16ef13a3421c675111e8a5d8768c7bd6eb0
Fishuuui <161544890+Fishuuui@users.noreply.github.com>
2024-03-18 09:03:38 -0700

    fixing a bug in the login system

--- main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Login.js
+++ main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Login.js
@@ -5,16 +5,16 @@ function checkUser(){
         location.href = "http://fishuuui.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
         }
       } else {
-      alert('Wrong Passcode or Username');
+      //alert('Wrong Passcode or Username');
        return false;
      }
 
     if(document.getElementById('username').value == 'crit'){
-        if(document.getElementById('passcode').value == 'crit'){
-            location.href = "http://fishuuui.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";  
+      if(document.getElementById('passcode').value == 'crit'){
+        location.href = "http://fishuuui.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
         }
     } else {
-        alert('Wrong Passcode or Username');
+        //alert('Wrong Passcode or Username');
         return false;
     }
 }
