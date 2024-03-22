6eee68eacb39fdd614ccde7ac433a2e350b91972
Fish <115324554+Bwaf@users.noreply.github.com>
2024-03-22 09:36:08 -0700

    kasdf

--- 12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/aV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Login.js
+++ 12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/aV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/Login.js
@@ -2,7 +2,7 @@ function checkUser(){
     if(document.getElementById('username').value == 'fish'){ 
        //alert('correct Passcode!');
        if(document.getElementById('passcode').value == 'fish'){
-        location.href = "http://project-blizzard.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
+        location.href = "http://project-blizzard.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
         }
         else {
       //alert('Wrong Passcode or Username');
@@ -12,7 +12,7 @@ function checkUser(){
 
     if(document.getElementById('username').value == 'chris'){
       if(document.getElementById('passcode').value == 'chris'){
-        window.location.href = "http://project-blizzard.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
+        window.location.href = "http://project-blizzard.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
         } else {
         //alert('Wrong Passcode or Username');
         return false;
@@ -20,7 +20,7 @@ function checkUser(){
     }
     if(document.getElementById('username').value == 'gecko'){
         if(document.getElementById('passcode').value == 'gecko'){
-            location.href = "http://project-blizzard.github.io/main/mainv2/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
+            location.href = "http://project-blizzard.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/chickenbutt/burgerwopperwithUturnSignsForLifeonchair/";
         } else {
         //alert{'Wrong Passcode or Username'};
         return false;
