8ea6edb10207faeab2b05c6c8ba31db64d26e4ff
fishychris1 <161517014+fishychris1@users.noreply.github.com>
2024-03-25 11:57:00 -0700

    add link and remove annoyances

--- 12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/aV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/adminstaff/index.js
+++ 12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/aV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/adminstaff/index.js
@@ -19,7 +19,7 @@ var password;
 var pass1="fishy";
 password=prompt('Enter Password To View Page','');
 if (password==pass1) {
-    alert('Correct password, Click OK to enter');
+    //alert('Correct password, Click OK to enter');
 }
 else {
     alert('Wrong, try again');
